81bfe834cf672d41d9d9232084fed723737e5d38
appablanca <feyzieren@gmail.com>
2023-07-11 16:28:02 +0300

    git commit -m "day2/nodejs/routes.js"

--- day2/nodejs/routes.js
+++ day2/nodejs/routes.js
@@ -13,6 +13,7 @@ const requestHandler = (req,res) =>{
     }
     
     if(url === "/message" && method === "POST"){
+        
         const body = [];
         req.on("data", chunk => {
             console.log(chunk);
