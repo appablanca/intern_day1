ff05d55e50fbe61429abbc36cfdee09b8b06e56a
appablanca <feyzieren@gmail.com>
2023-07-14 11:47:40 +0300

    git commit -m "day5"

---
+++ day5/mvc/models/product.js
@@ -0,0 +1,33 @@
+const { deepStrictEqual } = require('assert');
+const fs =  require('fs');
+const path = require('path');
+
+module.exports = class Product {
+    constructor(t) {
+        this.title = t;
+    }
+
+    save() {
+        const p = path.join(
+            path.dirname(require.main.filename)
+            ,"data"
+            ,"products.json"
+            );
+        fs.readFile(p, (err, fileContent) =>{
+            let products = [];
+            if(!err){
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) =>{
+                console.log(err);
+            });
+        });
+
+    }
+
+    static fetchAll() {
+        return products;
+    }
+
+}
