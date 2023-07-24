5c14d32306b87081a5e7944724606cb63d96175f
appablanca <feyzieren@gmail.com>
2023-07-24 10:55:27 +0300

    git commit -m "day11 start"

---
+++ day11 /errorHandling/routes/auth.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { check, body } = require('express-validator');
+
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+router.get('/login', authController.getLogin);
+
+router.get('/signup', authController.getSignup);
+
+router.post('/login', authController.postLogin);
+
+router.post('/signup',
+    [
+        check("email")
+            .isEmail()
+            .withMessage("Please enter a valid email.")
+            .custom((value, {req}) => {
+                return User.findOne({ email: value }).then(userDoc => {
+                    if (userDoc) {
+                        return Promise.reject(
+                            'E-Mail exists already, please pick a different one.'
+                        );
+                    }
+                });
+            }),
+        body(
+            "password",
+            "Please enter a password with only numbers and text and at least 6 characters"
+
+        )
+            .isLength({min: 6})
+            .isAlphanumeric(),
+        body("confirmPassword").custom((value, {req}) => {
+            if(value !== req.body.password){
+                throw new Error("Passwords have to match!");
+            }
+            return true;
+        })
+    ], 
+    authController.postSignup
+);
+
+router.post('/logout', authController.postLogout);
+
+module.exports = router;
