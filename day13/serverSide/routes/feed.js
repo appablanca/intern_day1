4c55f5cc30c5d04ced0dd2e1e70a73d4e11b51ae
appablanca <feyzieren@gmail.com>
2023-07-27 11:12:13 +0300

    git commit -m "day14 mid"

--- day13/serverSide/routes/feed.js
+++ day13/serverSide/routes/feed.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { body } = require("express-validator");
 const feedController =  require("../controllers/feed");
 
-
 const router = express.Router();
 
 router.get("/posts", feedController.getPosts);
